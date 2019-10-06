112615fdc51513a4d1eb445959b17bc3e475932e
Nicholas Jamieson <nicholas.jamieson@rexsoftware.com.au>
2019-10-07 08:26:05 +1000

    chore: Log webpack plugin names.

--- custom-webpack.config.js
+++ custom-webpack.config.js
@@ -1,4 +1,15 @@
 module.exports = (config, options) => {
   console.log('Loaders configured for', config.module.rules.map(({ test }) => test));
+  console.log('Plugins', config.plugins.map(plugin => {
+    const prototype = Object.getPrototypeOf(plugin);
+    if (!prototype) {
+      return '?';
+    }
+    const { constructor } = prototype;
+    if (!constructor) {
+      return '?';
+    }
+    return constructor.name || '?';
+  }));
   return config;
 };
