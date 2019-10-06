a5947de73cc4d85e6a9b6baaf03d3ec1d46b31e6
Nicholas Jamieson <nicholas.jamieson@rexsoftware.com.au>
2019-10-07 08:53:30 +1000

    chore: Wire up a do-nothing transformer.

--- custom-webpack.config.js
+++ custom-webpack.config.js
@@ -1,6 +1,6 @@
 module.exports = (config, options) => {
   console.log('Loaders configured for', config.module.rules.map(({ test }) => test));
-  console.log('Plugins', config.plugins.map(plugin => {
+  const names = config.plugins.map(plugin => {
     const prototype = Object.getPrototypeOf(plugin);
     if (!prototype) {
       return '?';
@@ -10,6 +10,23 @@ module.exports = (config, options) => {
       return '?';
     }
     return constructor.name || '?';
-  }));
+  });
+  const name = 'AngularCompilerPlugin';
+  const index = names.indexOf(name);
+  if (index === -1) {
+    console.warn(`${name} not found`);
+    return config;
+  }
+  const plugin = config.plugins[index];
+  const transformers = plugin._transformers;
+  if (!transformers) {
+    console.warn(`${name}#_transformers not found`);
+    return config;
+  }
+  console.log(`Found ${transformers.length} transformer(s)`);
+  transformers.push(context => sourceFile => {
+    console.log(`Transform ${sourceFile.fileName}`);
+    return sourceFile;
+  });
   return config;
 };
