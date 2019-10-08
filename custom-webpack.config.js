b748541d65e1a8540c1f9543e44f3f93091561f5
Nicholas Jamieson <nicholas.jamieson@rexsoftware.com.au>
2019-10-08 12:41:21 +1000

    chore: Wire up the configurer.

--- custom-webpack.config.js
+++ custom-webpack.config.js
@@ -1,34 +1,6 @@
+const { createAngularConfigurer } = require('../../dist/transformer');
+
 module.exports = (config, options) => {
-  console.log('Loaders configured for', config.module.rules.map(({ test }) => test));
-  const names = config.plugins.map(plugin => {
-    const prototype = Object.getPrototypeOf(plugin);
-    if (!prototype) {
-      return '?';
-    }
-    const { constructor } = prototype;
-    if (!constructor) {
-      return '?';
-    }
-    return constructor.name || '?';
-  });
-  const name = 'AngularCompilerPlugin';
-  const index = names.indexOf(name);
-  if (index === -1) {
-    console.warn(`${name} not found`);
-    return config;
-  }
-  const plugin = config.plugins[index];
-  const transformers = plugin._transformers;
-  if (!transformers) {
-    console.warn(`${name}#_transformers not found`);
-    return config;
-  }
-  console.log(`Found ${transformers.length} transformer(s)`);
-  // Angular's transformers use the type checker, so it's likely unsafe to
-  // insert the tools transformer before them.
-  transformers.push(context => sourceFile => {
-    console.log(`Transform ${sourceFile.fileName}`);
-    return sourceFile;
-  });
-  return config;
+  const configurer = createAngularConfigurer({});
+  return configurer(config, options);
 };
