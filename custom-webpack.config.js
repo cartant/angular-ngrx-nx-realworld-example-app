4df6ed35cf4300a6beeb43a7d74b13465b406458
Nicholas Jamieson <nicholas.jamieson@rexsoftware.com.au>
2019-10-09 16:51:27 +1000

    chore: Use supplied config module.

--- custom-webpack.config.js
+++
@@ -1,6 +0,0 @@
-const { createAngularConfigurer } = require('../../dist/transformer');
-
-module.exports = (config, options) => {
-  const configurer = createAngularConfigurer({});
-  return configurer(config, options);
-};
